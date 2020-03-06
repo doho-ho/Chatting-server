b3dc6c9b5b75e6bc2fd412a6ce1911a8e1c69cc7
Domo Lee <edh1021@gmail.com>
2020-03-06 22:02:23 +0900

    주석 제거본 재 업로드
    예전 소스코드 주석처리 부분 삭제

--- Source code/Map.h
+++ Source code/Map.h
@@ -2,72 +2,6 @@
 
 class chatServer;
 class player;
-/*
-struct msgSector
-{
-	msgSector()
-	{
-		sectorCount = 0;
-		int count = 0, count1 =0;
-		for (count = 0; count < 25; count++)
-		{
-			for (count1 = 0; count1 < 2; count1++)
-				Sector[count][count1] = 0;
-		}
-	}
-	int Sector[25][2];
-	int sectorCount;
-};
-
-struct Tile
-{
-	std::map<unsigned __int64, player*> *playerList = new std::map<unsigned __int64, player*>;
-};
-
-struct Sector
-{
-	Tile **sec;
-};
-
-class Map
-{
-private:
-	int		chSize;
-	int		sectorSize;
-
-	Sector *CH;
-
-	chatServer *server;
-	memoryPool<msgSector> msgPool;
-
-	SRWLOCK mapLock;		// CHÀÇ SRWLOCK
-
-private:
-	void loadConfig(const char *_configData);
-
-	bool deleteSector(unsigned __int64 _playerCode, int _CH, int _prevXpos, int _prevYpos);
-	bool insertSector(unsigned __int64 _playerCode, int _CH, int _destXpos, int _destYpos, player *_user);
-
-	msgSector* getSector(int _xPos, int _yPos);
-	void sendMsgToSector(unsigned __int64 _playerCode, int _CH, int _xPos, int _yPos, Sbuf *_buf, bool _selfSendFlag);
-	void sendPlayerData(unsigned __int64 _playerCode, int _CH, int _xPos, int _yPos);
-	void proc_sendMsg(unsigned __int64 _playerCode, int _CH, int _xPos, int _yPos, Sbuf *_buf, bool _selfSendFlag);
-	void proc_otherPlayerMsg(unsigned __int64 _playerCode, int _CH, int _xPos, int _yPos);
-
-public:
-	Map(const char *_configData, chatServer *_server);
-	~Map();
-
-	bool	updatePlayerPosition(player *_user, int _destX, int _destY);
-	bool	updatePlayerCH(player *_user, int _destCH, bool _firstInsert = false);
-	bool	deletePlayer(player *_user);
-
-	void sendMsg(player *_user, Sbuf *_buf, bool _selfSendFlag = false);
-
-	void sendOtherPlayerData(player *_User);
-	
-};
-*/
 
 struct tileCollection
 {
@@ -105,7 +39,6 @@ private:
 	
 	tileCollection getAroundTile(int _xTile, int _yTile);
 	tileCollection getCalTile(int _prevXTile, int _prevYTile, int _destXTile, int _destYTile, bool _insertFlag);
-	// prev -1 : insert¸¸ , dest : -1 : delete¸¸.
 
 	void sendUpdateMsg(unsigned __int64 _playerCode, int _prevXTile, int _prevYTile, int _destXTile, int _destYTile);
 	void sendMsg(unsigned __int64 _playerCode, tileCollection _Collection, bool _insertFlag, Sbuf *_buf);
